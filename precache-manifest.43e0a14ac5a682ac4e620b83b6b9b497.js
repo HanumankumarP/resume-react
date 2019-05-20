dd4ff5a6fe2ddbce6018de44be10f212e26e904f
nagapravallika83 <46373476+nagapravallika83@users.noreply.github.com>
2019-05-20 16:59:17 +0530

    Updates

---
+++ precache-manifest.43e0a14ac5a682ac4e620b83b6b9b497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe735fc300606cfcf9318809c2c6111",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "58ccafb7e9d45443ed77",
+    "url": "/resume/static/css/main.f3df65a1.chunk.css"
+  },
+  {
+    "revision": "3c764f355ca3f31c3815",
+    "url": "/resume/static/js/2.b44a9c0d.chunk.js"
+  },
+  {
+    "revision": "58ccafb7e9d45443ed77",
+    "url": "/resume/static/js/main.313aec3a.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "2a42d7b274bb96d8cc0976555277bea8",
+    "url": "/resume/static/media/profile.2a42d7b2.png"
+  }
+]);
